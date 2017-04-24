23cea72dca5f7b95403c6484a79ec18d1b5497c9
Oskar Ek <oskarek24@gmail.com>
2017-04-24 18:28:24 +0200

    timer

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import net.librec.filter.GenericRecommendedFilter;
 import net.librec.recommender.Recommender;
 import net.librec.recommender.RecommenderContext;
 import net.librec.recommender.cf.ItemKNNRecommender;
+import net.librec.recommender.cf.UserKNNRecommender;
 import net.librec.recommender.cf.rating.SVDPlusPlusRecommender;
 import net.librec.recommender.ext.SlopeOneRecommender;
 import net.librec.recommender.item.RecommendedItem;
@@ -16,61 +17,66 @@ import net.librec.similarity.RecommenderSimilarity;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by oskarek on 2017-04-22.
  */
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // build data model
-        Configuration conf = new Configuration();
-        conf.set("data.input.path","movielens");
-        conf.set("dfs.data.dir", "/Users/oskarek/Documents/Skola/Kex/librec/data");
-        TextDataModel dataModel = new TextDataModel(conf);
-        dataModel.buildDataModel();
+  public static void main(String[] args) throws Exception {
+    // build data model
+    Configuration conf = new Configuration();
+    conf.set("data.input.path","movielens");
+    conf.set("dfs.data.dir", "/Users/oskarek/Documents/Skola/Kex/librec/data");
+    Configuration.Resource resource = new Configuration.Resource("rec/cf/userknn-test.properties");
+    conf.addResource(resource);
+    TextDataModel dataModel = new TextDataModel(conf);
+    dataModel.buildDataModel();
 
-        // build recommender context
-        RecommenderContext context = new RecommenderContext(conf, dataModel);
+    // build recommender context
+    RecommenderContext context = new RecommenderContext(conf, dataModel);
 
-        // build similarity
-        conf.set("rec.recommender.similarity.key" ,"item");
-        RecommenderSimilarity similarity = new PCCSimilarity();
-        similarity.buildSimilarityMatrix(dataModel);
-        context.setSimilarity(similarity);
+    // build similarity
+    conf.set("rec.recommender.similarity.key" ,"item");
+    RecommenderSimilarity similarity = new PCCSimilarity();
+    similarity.buildSimilarityMatrix(dataModel);
+    context.setSimilarity(similarity);
 
-        // build recommender
-        Recommender recommender = new SlopeOneRecommender();
-        recommender.setContext(context);
+    long startTime = System.nanoTime();
 
-        // run recommender algorithm
-        recommender.recommend(context);
+    // build recommender
+    Recommender recommender = new UserKNNRecommender();
+    recommender.setContext(context);
 
-        // evaluate the recommended result
-        RecommenderEvaluator evaluator = new RMSEEvaluator();
-        System.out.println("RMSE: " + recommender.evaluate(evaluator));
 
-        MAEEvaluator maeEvaluator = new MAEEvaluator();
-        System.out.println("MAE: " + recommender.evaluate(maeEvaluator));
 
-        // set id list of filter
-        List<String> userIdList = new ArrayList<>();
-        List<String> itemIdList = new ArrayList<>();
-        userIdList.add("1");
-        itemIdList.add("70");
+    // run recommender algorithm
+    recommender.recommend(context);
 
-        // filter the recommended result
-        List<RecommendedItem> recommendedItemList = recommender.getRecommendedList();
-        GenericRecommendedFilter filter = new GenericRecommendedFilter();
-        filter.setUserIdList(userIdList);
-        filter.setItemIdList(itemIdList);
-        recommendedItemList = filter.filter(recommendedItemList);
+    startTime = TimeUnit.NANOSECONDS.toMillis(startTime);
 
-        // print filter result
-        for (RecommendedItem recommendedItem : recommendedItemList) {
-            System.out.println(
-            "user:" + recommendedItem.getUserId() + " " +
-            "item:" + recommendedItem.getItemId() + " " +
-            "value:" + recommendedItem.getValue()
-            );
-        }
-    }
+    System.out.println("Time elapsed: " + (TimeUnit.NANOSECONDS.toMillis(System.nanoTime()) - startTime));
+
+    // evaluate the recommended result
+    RecommenderEvaluator evaluator = new RMSEEvaluator();
+    System.out.println("RMSE: " + recommender.evaluate(evaluator));
+
+    MAEEvaluator maeEvaluator = new MAEEvaluator();
+    System.out.println("MAE: " + recommender.evaluate(maeEvaluator));
+
+    // set id list of filter
+    List<String> userIdList = new ArrayList<>();
+    List<String> itemIdList = new ArrayList<>();
+    userIdList.add("1");
+    itemIdList.add("70");
+
+    // filter the recommended result
+    List<RecommendedItem> recommendedItemList = recommender.getRecommendedList();
+    GenericRecommendedFilter filter = new GenericRecommendedFilter();
+    filter.setUserIdList(userIdList);
+    filter.setItemIdList(itemIdList);
+    recommendedItemList = filter.filter(recommendedItemList);
+
+  }
 }
